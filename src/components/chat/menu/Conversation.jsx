0f59a9b3b46153bdca809921a0b04633bd7a269a
cjuanik <anik.patra2014@gmail.com>
2023-02-16 23:52:56 +0530

    Update Conversation Window

---
+++ src/components/chat/menu/Conversation.jsx
@@ -0,0 +1,31 @@
+import { Box, Typography,styled } from "@mui/material";
+
+const Component = styled(Box)`
+    display: flex;
+    height: 45px;
+    padding: 13px 0;
+    cursor: pointer;
+`
+const Image = styled('img')({
+    width: 50,
+    height: 50,
+    borderRadius: '50%',
+    padding: '0 14px'
+})
+
+const Conversation = ({user}) => {
+    console.log('Conversation got mounted');
+    return(
+        <Component>
+            <Box>
+                <Image src={user.picture} alt="dp"/>
+            </Box>
+            <Box>
+                <Box>
+                    <Typography>{user.name}</Typography>
+                </Box>
+            </Box>
+        </Component>
+    )
+}
+export default Conversation;
