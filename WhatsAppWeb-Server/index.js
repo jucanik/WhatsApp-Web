2aa4820b6dc1495babfe26d4891d947acd6f175c
cjuanik <anik.patr2014@gmail.com>
2023-02-17 23:56:09 +0530

    Updated Server Index file

--- WhatsAppWeb-Server/index.js
+++ WhatsAppWeb-Server/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import Connection from './database/db.js';
+import Route from './route/route.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
-const PORT = 7001;
 
+app.use(cors());
+
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/', Route);
+
+const PORT = 7001;
+Connection();
 app.listen(PORT, () =>{
-    console.log(`App is running successfully on PORT ${PORT}`)
+    console.log(`App is running successfully on PORT ${PORT}`);
 })
