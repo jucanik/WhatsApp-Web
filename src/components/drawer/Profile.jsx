d938dbf1c46e2eceb0ff74be99d158d64b6afd68
cjuanik <anik.patra2014@gmail.com>
2023-02-20 23:45:28 +0530

    Add Profile Component

---
+++ src/components/drawer/Profile.jsx
@@ -0,0 +1,74 @@
+import { Box, styled, Typography } from "@mui/material";
+import { useContext } from "react";
+import { AccountContext } from "../../context/AccountProvider";
+
+const ImageContainer = styled(Box)`
+    display: flex;
+    justify-content: center;
+`
+const Image = styled('img')({
+    width: 200,
+    height: 200,
+    borderRadius: '50%',
+    padding: '25px 0' 
+}) 
+
+const NameContainer = styled(Typography)`
+    background: #FFFFFF;
+    padding: 12px 30px 2px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.08);
+    & :first-child {
+        font-size: 13px;
+        color: #009688;
+        font-weight: 200;
+    }
+    & :last-child {
+        margin: 14px 0;
+        color: #4A4A4A;
+    }
+`
+
+const DescriptionContainer = styled(Box)`
+    padding: 15px 20px 28px 30px;
+    & > p {
+        font-size: 13px;
+        color: #8696a0;
+    }
+`
+const StatusContainer = styled(Box)`
+    background: #FFFFFF;
+    padding: 12px 30px 2px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.08);
+    & :first-child {
+        font-size: 13px;
+        color: #009688;
+        font-weight: 200;
+    }
+    & :last-child{
+        padding: 14px 0;
+        color: #4A4A4A;
+    }
+`
+const Profile = () => {
+
+    const {account} = useContext(AccountContext);
+    return(
+        <>
+            <ImageContainer>
+                <Image src={account.picture} alt ="dp"/>
+            </ImageContainer>
+            <NameContainer>
+                <Typography>Your Name</Typography>
+                <Typography>{account.name}</Typography>
+            </NameContainer>
+            <DescriptionContainer>
+                <Typography>This is your name will be shown to your Whatsapp Contacts</Typography>
+            </DescriptionContainer>
+            <StatusContainer>
+                <Typography>About</Typography>
+                <Typography>Eat Sleep Code Repeat!</Typography>
+            </StatusContainer>
+        </>
+    )
+}
+export default Profile;
